948df07d9eaacf4b7b8d302e6512ba183fcb5c66
Droffyzin <59933114+messiribeiro@users.noreply.github.com>
2023-05-24 12:17:12 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,76 @@
 const input = document.querySelector("#clipInput");
+const textarea = document.querySelector(".comment-textarea")
 const form = document.querySelector("#form")
 const inputContainer = document.querySelector(".form-container");
 const message = document.querySelector("#message");
+const changeIdForm = document.querySelector("#changeId");
+
+const configContainer =  document.querySelector("#config-container");
+const configButton = document.querySelector("#settings")
+const inputId = document.querySelector("#channelID");
+
+const updateView = document.querySelector("#update-view");
 
 
+if(!(localStorage.getItem("updateView"))) {
+    updateView.style.display = "flex"
+    localStorage.setItem("updateView", true)
+}
+
+
+updateView.addEventListener("click", (event) => {
+    if (event.target === updateView) {
+        updateView.style.display = "none"
+    }
+  });
+
+
+
+if(!(localStorage.getItem("channelId"))) localStorage.setItem("channelId", 1110178126631272559)
+
+
+textarea.addEventListener("keydown", (e) => {
+    if(e.key === "Enter" && !e.shiftKey){
+        textarea.blur()
+        submit();
+
+    }
+})
+
+
+
+configButton.addEventListener("click", () => {
+    configContainer.classList.add("openconfig");
+    configContainer.classList.remove("closeconfig")
+    
+})
+
+configContainer.addEventListener("click", (event) => {
+    if (event.target === configContainer) {
+        
+        configContainer.classList.add("closeconfig");
+        configContainer.classList.remove("openconfig")
+
+
+        
+    }
+  });
 input.addEventListener("focus", () => {
     inputContainer.classList.add("form-container-focus")
     inputContainer.classList.remove("form-container")
-
-
-
 })
+
+
+textarea.addEventListener("focus", () => {
+    textarea.classList.add("comment-textarea-focus");
+    textarea.classList.remove("comment-textarea");
+})
+
+textarea.addEventListener("blur", () => {
+    textarea.classList.remove("comment-textarea-focus")
+    textarea.classList.add("comment-textarea");
+})
+
 
 input.addEventListener("blur", () => {
     inputContainer.classList.remove("form-container-focus")
@@ -19,42 +79,66 @@ input.addEventListener("blur", () => {
 
 })
 
-form.addEventListener("submit", async (event) => {
-    if(input.value == "") {
-        alert("Bota alguma coisa ae garai")
-    }else{
-        const url = 'https://addclip.onrender.com/addClip';
+
+inputId.value = localStorage.getItem("channelId");
+
+changeIdForm.addEventListener("submit", (e) => {
+
+    if(inputId.value) {
+        localStorage.setItem("channelId", inputId.value);
+        e.preventDefault()
+        configContainer.classList.add("closeconfig");
+        configContainer.classList.remove("openconfig")
+
+    }
+    
+})
+
+function submit(event){
+    if (input.value == "") {
+        alert("Campo vazio!")
+    } else {
+        const url = 'http://localhost:3000/addClip';
 
         const clip = {
-            link: input.value
+            link: input.value,
+            channelId: inputId.value,
+            description: textarea.value
         }
+        
         console.log(JSON.stringify(clip))
 
         const options = {
             method: 'POST',
             headers: {
-              'Content-Type': 'application/json'
+                'Content-Type': 'application/json'
             },
             body: JSON.stringify(clip)
-          };
-          
-          fetch(url, options)
+        };
+
+        fetch(url, options)
             .then(response => response.json())
             .then(data => console.log(data))
             .catch(error => console.error(error));
-        input.value='';
+        input.value = '';
+        textarea.value = '';
+        // textarea.setAttribute("placeholder", "Adicione um comentário (Opcional)")
+        startAnimation();
+
     }
 
     event.preventDefault();
-    startAnimation();
 
-    
+}
+form.addEventListener("submit", async (event) => {
+    submit(event); 
+
 })
- function startAnimation(){
+function startAnimation() {
     message.style.display = "flex";
     setTimeout(() => {
         message.style.display = "none"
     }, 985)
- }
+}
 
 
